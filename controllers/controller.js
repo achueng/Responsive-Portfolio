9053f6baf851b3f6d4856a3e12d6cb990356ec87
Agnes Chueng <agneschueng@gmail.com>
2020-11-06 16:13:53 -0600

    Get route to html files

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const dirHTML = path.join(__dirname, "../public/")
 
 // Code to get and post goes here
+router.get("/", function(req, res) {
+    res.sendFile(path.join(dirHTML, "index.html"));
+});
 
-module.exports = routes;
+router.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(dirHTML, "portfolio.html"));
+});
+
+router.get("/contact", function(req, res) {
+    res.sendFile(path.join(dirHTML, "contact.html"));
+});
+
+module.exports = router;
