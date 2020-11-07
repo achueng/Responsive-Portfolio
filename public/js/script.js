07d8cce980dfac75f506da91e2d0d09fc8cb5271
Agnes Chueng <agneschueng@gmail.com>
2020-11-06 18:03:37 -0600

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Get all the repositories on the github for the user: achueng
+    // const queryUrl = "https://api.github.com/users/achueng/repos?per_page=100";
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET"
+    // }).then(function(data){
+    //     console.log(data);
+    // });
+});
